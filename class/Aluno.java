8ea039e7d116b58339a248b933381aa0bf0ae0d3
samuelrsales <samuelsales943@gmail.com>
2026-06-04 21:03:46 -0300

    class Aluno added

---
+++ class/Aluno.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+
+public class Aluno {
+    private String nome;
+    private String email;
+    private int matricula;
+    private ArrayList<Double> notas;
+
+    public Aluno(String nome, String email, int matricula) {
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public int getMatricula() {
+        return this.matricula;
+    }
+
+    public void getNotas() {
+        for (double nota: this.notas) {
+            System.out.println(nota);
+        }
+    }
+
+    public void adicionarNota(double nota) {
+        this.notas.add(nota);
+    }
+
+    public boolean verificarAprovado() {
+        double media = this.notas.stream().mapToDouble(Double::doubleValue).average().orElse(0);
+
+        if (media >= 7) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+
+}
